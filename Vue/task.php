7e51f20316b6dc9213bc36774c8fd158a664d443
Kylian <kylian.patry@gmail.com>
2024-10-14 15:59:16 +0200

    add msg

--- Vue/task.php
+++ Vue/task.php
@@ -52,6 +52,11 @@
     </card>
 
   </div>
+  <div>
+    <hr>
+    <p class="text-centergti">MyDigitalSchool 2024</p>
+  </div>
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
